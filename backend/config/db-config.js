3055735ec33202f51da80aef34c35bb3fde09358
Lejoy <lejoy.j@jmangroup.com>
2023-09-23 16:19:57 +0530

    dadadada

--- backend/config/db-config.js
+++ backend/config/db-config.js
@@ -1,15 +1,8 @@
 module.exports= {
-
     USER: "root",
-  
     PASSWORD: "1234",
-  
     HOST: "127.0.0.1",
-  
     PORT: "3306",
-  
     DATABASE: "training_planner_db",
-  
     DIALECT: "mysql",
-  
-  };
+};
